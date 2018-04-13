3badf61a5455ec4e57465672a4fbed6db20b3d9e
Javier Munoz Fernandez <jamu620@gmail.com>
2018-04-13 16:54:31 -0400

    Some Sort of Update

--- Test.java
+++ Test.java
@@ -7,8 +7,11 @@ import java.text.NumberFormat;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.io.BufferedInputStream;
 
 public class Test {
+	private static final Scanner STDIN =
+            new Scanner(new BufferedInputStream(System.in));
     public static void main(final String[] args) throws FileNotFoundException {
         final File inputFile = new File(args[0]);
         final Scanner inputData = new Scanner(inputFile);
@@ -19,22 +22,70 @@ public class Test {
                 tree.insert(str.toUpperCase());
             }
         }
-        tree.display();
-		
+        //tree.display();
         long memory = peakMemoryUsage();
         System.out.println(NumberFormat.getNumberInstance(Locale.US).format(memory));
     }
     private static long peakMemoryUsage() {
 
     List<MemoryPoolMXBean> pools = ManagementFactory.getMemoryPoolMXBeans();
-    long total = 0;
-    for (MemoryPoolMXBean memoryPoolMXBean : pools) {
-        if (memoryPoolMXBean.getType() == MemoryType.HEAP) {
-            long peakUsage = memoryPoolMXBean.getPeakUsage().getUsed();
-            // System.out.println("Peak used for: " + memoryPoolMXBean.getName() + " is: " + peakUsage);
-            total = total + peakUsage;
-        }
+		long total = 0;
+		for (MemoryPoolMXBean memoryPoolMXBean : pools) {
+			if (memoryPoolMXBean.getType() == MemoryType.HEAP) {
+				long peakUsage = memoryPoolMXBean.getPeakUsage().getUsed();
+				// System.out.println("Peak used for: " + memoryPoolMXBean.getName() + " is: " + peakUsage);
+				total = total + peakUsage;
+			}
     }
     return total;
     }
+	public static MyList<String> findAllPossibleWords(final Trie tree, final char[] tilesInHand, final char[] tilesOnBoard,
+													  final int spaceBefore, final int spaceAfter, final MyList<String> list) {
+		for (char c : tilesOnBoard) {
+			tree.addWords(c, list, spaceBefore, spaceAfter, tilesInHand);
+		}
+		return null;
+	}
+	 //call this method
+	  public static ScrabbleWord findWord(char[][] board) {
+		for (int row = 0; row < board.length; row ++) {
+			for (int col = 0; col < board.length; col++) {
+				if (board[row][col] != ' ') {
+					char orientation;
+					if (board[row][col + 1] != ' ') {
+						orientation = 'h';
+					} else if (board[row + 1][col] != ' ') {
+						orientation = 'v';
+					} else {
+						orientation = 'o';
+
+					}
+					String word = getWord(orientation, board, row, col);
+					return new ScrabbleWord(word, row, col, orientation);
+				}
+			}
+		}
+		return null;
+	  }
+
+	//this method only supports findWord
+	public static String getWord(char orientation, char[][] board, int row, int col) {
+		if (orientation == 'h') {
+			int wordSize = board.length - col;
+			String word = "";
+			for (int c = 0; c < wordSize; c++) {
+				word = word.concat(Character.toString(board[row][col + c]));
+			}
+			return word;
+		} else if (orientation == 'v'){
+			int wordSize = board.length - row;
+			String word = "";
+			for (int r = 0; r < wordSize; r++) {
+				word = word.concat(Character.toString(board[row + r][col]));
+			}
+			return word;
+		} else {
+			return Character.toString(board[row][col]);
+		}
+	}
 }
--- Trie.java
+++ Trie.java
@@ -1,12 +1,22 @@
 
 public class Trie {
-    private static class Entry implements Comparable<Entry> {
+    public static class Entry implements Comparable<Entry> {
         char element;
         boolean isWordEnd = false;
+		final int prefixLength;
         MyList<Entry> children = new MyList<Entry>();
-        Entry(final char theElement) {
+        Entry(final char theElement, final Entry theParent) {
             element = theElement;
+			parent = theParent;
+			if (parent == null) {
+				prefixLength = -1;
+			} else {
+				prefixLength = parent.prefixLength + 1;
+			}
         }
+		Entry(final char theElement) {
+			this(theElement, null);
+		}
         @Override
         public int compareTo(Entry other) {
             return Character.compare(element, other.element);
@@ -28,7 +38,7 @@ public class Trie {
     private void insert(final String word, final int index, final Entry current) {
         final MyList<Entry> kids = current.children;
         final char element = word.charAt(index);
-        final Entry next = kids.insert(new Entry(element));
+        final Entry next = kids.insert(new Entry(element), current);
         if (index < word.length() - 1) {
             insert(word, index + 1, next);
         } else {
@@ -37,10 +47,7 @@ public class Trie {
 
     }
     public void display() {
-        display(root);
-    }
-    public static void display(final Entry e) {
-		display(e, "");
+        display(root, "");
     }
 	private static void display(final Entry e, final String s) {
 		if (e.isWordEnd) {
@@ -50,5 +57,19 @@ public class Trie {
 			display(c, s + e);
 		}
 		//System.out.println();
+	}
+	public void addWords(char c, MyList<String> list, int spaceBefore, int spaceAfter, byte[] hand) {
+		for (Entry e : root.children) {
+			if (e.element == 'c') {
+				addWords(e, list, spaceBefore, spaceAfter, hand, c);
+			}
+		}
+	}
+	private void addWords(Entry e, MyList<String> list,  int spaceBefore, int spaceAfter, byte[] hand, String s) {
+		for (Entry c : e.children) {
+			if (c.element == 'c') {
+				addWords(c, list, spaceBefore, spaceAfter, hand, c);
+			}
+		}
 	}
 }
